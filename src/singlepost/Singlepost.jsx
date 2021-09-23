755dc907d9a2060f75d6484dc40a03e787ba2a63
Thathsarani Nimsara Narasinghe Bandara <89225439+NimsaraVTA@users.noreply.github.com>
2021-09-24 02:44:23 +0530

    Add files via upload

---
+++ src/singlepost/Singlepost.jsx
@@ -0,0 +1,53 @@
+import './singlepost.css';
+
+export default function Single() {
+
+    return (
+
+
+        <div className="singlepost">
+
+            <div className="singlePostWrapper">
+
+                <img className="singlePostImg" src="http://images.unsplash.com/photo-1505816014357-96b5ff457e9a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" alt="" />
+
+          
+
+            <h1 className="singlePostTitle">Sigle Post Title
+
+                <div className="singlePostEdit">
+                    <i className="singlePostIcon far fa-edit"></i>
+                    <i className="singlePostIcon far fa-trash-alt"></i>
+                </div>
+
+            </h1>
+
+            <div className="singlePostInfo">
+
+                <span className="singlePostAuthor">Author: <strong>Thathsarani</strong></span>
+                <span className="singlePostDate">1 hour ago</span>
+
+            </div>
+
+            <p className="singlePostDes">The next generation of the web favorite icon library + toolkit is now available
+                as a Beta release! Subscribe to Font Awesome Pro and get instant access to the
+                v6 beta
+                The next generation of the web favorite icon library + toolkit is now available
+                as a Beta release! Subscribe to Font Awesome Pro and get instant access to the
+                v6 beta
+                The next generation of the web favorite icon library + toolkit is now available
+                as a Beta release! Subscribe to Font Awesome Pro and get instant access to the
+                v6 beta
+                The next generation of the web favorite icon library + toolkit is now available
+                as a Beta release! Subscribe to Font Awesome Pro and get instant access to the
+                v6 beta
+                The next generation of the web favorite icon library + toolkit is now available
+                as a Beta release! Subscribe to Font Awesome Pro and get instant access to the
+                v6 beta
+            </p>
+
+        </div>
+        </div>
+    )
+
+}
