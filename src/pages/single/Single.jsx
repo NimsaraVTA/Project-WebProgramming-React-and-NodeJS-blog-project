7a64294fba5ebaa4383e5e19e1b1c11519ed845b
Thathsarani Nimsara Narasinghe Bandara <89225439+NimsaraVTA@users.noreply.github.com>
2021-09-24 02:37:38 +0530

    Add files via upload

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,19 @@
+import './single.css';
+import Sidebar from '../../sidebar/Sidebar';
+import Singlepost from '../../singlepost/Singlepost';
+
+export default function Single() {
+
+    return (
+
+     
+        <div className="single">
+
+            <Singlepost />
+            <Sidebar />
+
+            </div>
+
+    )
+
+}
