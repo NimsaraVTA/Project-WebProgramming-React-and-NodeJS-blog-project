5b818084fe282cced9d7a8fcd90bbfa528254ad8
Thathsarani Nimsara Narasinghe Bandara <89225439+NimsaraVTA@users.noreply.github.com>
2021-09-24 02:30:23 +0530

    Add files via upload

---
+++ src/header/Header.jsx
@@ -0,0 +1,19 @@
+import './header.css';
+
+export default function Header() {
+
+    return (
+
+        <div className="header">
+
+            <div className="headerTitles">
+                <span className="headerTitlesSm">Thathsarani Nimsara</span>
+                <span className="headerTitlesLg">React & Node Blog</span>
+            </div>
+
+            <img className="headerImg" src="https://miro.medium.com/max/1200/1*eDZYmIHwX5YmS2u2T78fHQ.jpeg" alt="" />
+        </div>
+        
+        )
+
+}
