7d736ff4dd6d1cd2fcee5b2c23632ea13714bf5c
Thathsarani Nimsara Narasinghe Bandara <89225439+NimsaraVTA@users.noreply.github.com>
2021-09-24 02:40:51 +0530

    Add files via upload

---
+++ src/posts/Posts.jsx
@@ -0,0 +1,21 @@
+import './posts.css';
+import Post from '../post/Post';
+
+export default function Posts() {
+
+    return (
+
+        <div className="posts">
+
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+
+        </div>
+
+    )
+
+}
